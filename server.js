a6711a27d24c5f0a661e052783c45defa85de8c1
Zurc <cruzjurado@gmail.com>
2017-05-20 00:53:50 +0100

    repeat-remember

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 function onRequest(request, response) {
     response.writeHead(200, {'Content-type': 'text/plain'});
-    response.write('Hello Cruz, this is Node JS server working');
+    response.write('Hello from Node Js server ');
     response.end();
 }
 
