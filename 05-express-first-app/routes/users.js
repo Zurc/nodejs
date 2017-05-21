80ee2ecb40aa981dd40b7063f85456ff18fcf901
Zurc <cruzjurado@gmail.com>
2017-05-21 06:48:27 +0100

    play: set another sub-route in users route

--- 05-express-first-app/routes/users.js
+++ 05-express-first-app/routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// if we want to navigate to localhost:8000/users/detail 
+router.get('/detail', function(req, res, next) {
+  res.send('This are user details');
+});
+
 module.exports = router;
