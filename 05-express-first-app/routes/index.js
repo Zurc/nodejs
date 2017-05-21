43eede9ea49b4fbd3c5bbe6f77c413d317eced17
Zurc <cruzjurado@gmail.com>
2017-05-21 12:03:11 +0100

    all routes in one file

--- 05-express-first-app/routes/index.js
+++ 05-express-first-app/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET users listing. */
+router.get('/users', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+// if we want to navigate to localhost:8000/users/detail 
+router.get('/users/detail', function(req, res, next) {
+  res.send('This are user details');
+});
+
 module.exports = router;
