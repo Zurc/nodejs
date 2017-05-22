96e7794d57b80185e13e3f1cd56296f682746ab5
Zurc <cruzjurado@gmail.com>
2017-05-22 22:10:36 +0100

    playing with handlebars templateing engine

---
+++ 06-handlebars/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Cool, huh!', condition: false, anyArray: [1,2,3] });
+});
+
+/* GET users listing. */
+router.get('/users', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+// if we want to navigate to localhost:8000/users/detail 
+router.get('/users/detail', function(req, res, next) {
+  res.send('This are user details');
+});
+
+module.exports = router;
