c1b87bbbcbf27283afbbbce79bc26162a85e3bd4
Zurc <cruzjurado@gmail.com>
2017-06-04 22:10:36 +0100

    validation enabled

--- 08-validation-session/app.js
+++ 08-validation-session/app.js
@@ -8,12 +8,12 @@ var hbs = require('express-handlebars');
 var expressValidator = require('express-validator');
 var expressSession = require('express-session');
 
-
 var routes = require('./routes/index');
+
 var app = express();
 
 // view engine setup
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}))
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -22,8 +22,10 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}));
 
 app.use('/', routes);
 
@@ -34,15 +36,29 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
--- 08-validation-session/routes/index.js
+++ 08-validation-session/routes/index.js
@@ -3,18 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cool, huh!', condition: false, anyArray: [1,2,3] });
+  res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors });
+  req.session.errors = null;
+  req.session.success = null;
 });
 
-router.get('/test/:id', function(req, res, next) {
-  // I get the param set on my route url (:id) with req.params.id
-  res.render('test', { output: req.params.id});
-})
+router.post('/submit', function(req, res, next) {
+  // check validity of your values
+  req.check('email', 'Invalid email address').isEmail();
+  req.check('password', 'Password is invalid').isLength({min: 4}).equals(req.body.confirmPassword);
 
-router.post('/test/submit', function(req, res, next) {
-    // I post the param set on my POST request body (id from the index.hbs form) with req.body.id
-  var id = req.body.id;
-  res.redirect('/test/' + id);
+  var errors = req.validationErrors();
+  if(errors) {
+    req.session.errors = errors;
+    req.session.success = false;
+  } else {
+    req.session.success = true;
+  }
+  res.redirect('/');
 });
-
 module.exports = router;
