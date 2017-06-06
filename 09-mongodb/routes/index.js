86aa7bbef9ebb7694493bbae007ee18996ebb3d2
Zurc <cruzjurado@gmail.com>
2017-06-07 00:33:54 +0100

    insert and get data working

--- 09-mongodb/routes/index.js
+++ 09-mongodb/routes/index.js
@@ -1,20 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Cool, huh!', condition: false, anyArray: [1,2,3] });
 });
 
-router.get('/test/:id', function(req, res, next) {
-  // I get the param set on my route url (:id) with req.params.id
-  res.render('test', { output: req.params.id});
+router.get('/get-data', function(req, res, next) {
+  var resultArray = [];
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var cursor = db.collection('user-data').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      res.render('index', {items: resultArray});
+    })
+  })
 })
 
-router.post('/test/submit', function(req, res, next) {
-    // I post the param set on my POST request body (id from the index.hbs form) with req.body.id
-  var id = req.body.id;
-  res.redirect('/test/' + id);
-});
+router.post('/insert', function(req, res, next) {
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('user-data').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      db.close();
+    })
+  })
+})
+
+router.post('/update', function(req, res, next) {
+
+})
+
+router.post('/delete', function(req, res, next) {
+
+})
+
 
 module.exports = router;
